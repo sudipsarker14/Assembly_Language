0d2b3d5df43b039ea54936b80cbbae69dea800fb
Sudip Sarker <113766948+sudipsarker14@users.noreply.github.com>
2024-01-11 17:10:47 +0600

    1+11+111

--- 1+11+111.asm
+++ 1+11+111.asm
@@ -21,6 +21,30 @@ code segment
  
  hlt
  ends
+
+'using loop
+
+org 100h 
+code segment
+    assume cs: code, ds: code
+    
+    mov ax, 1h
+    mov bx, 10d
+    mov cx, 3d
+    mov dx, 1h
+    
+    sdp:
+    
+    mul bl
+    add ax, 1
+    add dx, ax
+    loop sdp
+  
+ exit:
  
+ hlt
+ ends
+ 
+ ret
  ret
       
