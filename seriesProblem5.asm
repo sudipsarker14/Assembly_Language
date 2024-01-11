0b2bd37e769145af2c2915f35f68521a0f16d827
Sudip Sarker <113766948+sudipsarker14@users.noreply.github.com>
2024-01-11 16:16:25 +0600

    1+2^2+3^3+4^4

--- seriesProblem5.asm
+++ seriesProblem5.asm
@@ -45,3 +45,51 @@ code segment
     code ends
 end
 ret
+
+; using loop
+
+org 100h
+
+code segment
+    assume cs:code, ds: code
+   
+      mov dx, 1h
+      ;2^2
+      
+      mov ax, 1h
+      mov bx, 2h
+      mov cx, 2d
+      
+      pw2:  
+      
+      mul bl
+      loop pw2
+      add dx, ax  
+      
+     ;3^3
+      
+      mov ax, 1h
+      mov bx, 3h
+      mov cx, 3d
+      
+      pw3:
+      mul bl
+      loop pw3
+      add dx, ax
+                  
+      ;4^4
+      
+      mov ax, 1h
+      mov bx, 4h
+      mov cx, 4d
+      
+      pw4:
+      mul bl
+      loop pw4   
+      add dx, ax            
+      
+     
+    exit:
+    code ends
+end
+ret
