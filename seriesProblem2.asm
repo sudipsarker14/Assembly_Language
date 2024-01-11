05e90cc7c46cbb24e80a0624f3606c8c59a04561
Sudip Sarker <113766948+sudipsarker14@users.noreply.github.com>
2024-01-11 16:39:46 +0600

    2+4+6+8+....+n

--- seriesProblem2.asm
+++ seriesProblem2.asm
@@ -17,3 +17,23 @@ code segment
     code ends
 end
 ret
+
+'useing loop
+org 100h
+
+code segment
+    assume cs:code, ds: code
+   
+   mov ax, 0h 
+   mov bx, 2h
+   mov cx, 30d
+   
+   sdp:      
+   add ax, bx 
+   add bx, 2
+   loop sdp
+     
+    exit:
+    code ends
+end
+ret
